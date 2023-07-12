0e7c568af097d9e82e8cfd943f03a96b9fa80cf1
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-07-12 23:33:50 +0300

    [feature] implemented the auth protection guards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,40 @@
+import { lazy, Suspense } from "react";
 import Forbidden from "@/pages/403";
 import NotFound from "@/pages/404";
 import InternalServerError from "@/pages/500";
-import Login from "@/pages/auth/login";
 import { Routes, Route } from "react-router-dom";
+import { RequireUser, UserAuth } from "@/utils/guards";
+import { RouteLoader } from "@/components/styled";
+import { Loader } from "@/components/common";
+const Login = lazy(() => import("@/pages/auth/login"));
 
 function BaseRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/forbidden" element={<Forbidden />} />
+      <Route
+        path="/login"
+        element={
+          <UserAuth>
+            <Suspense
+              fallback={
+                <RouteLoader>
+                  <Loader size={50} />
+                </RouteLoader>
+              }
+            >
+              <Login />
+            </Suspense>
+          </UserAuth>
+        }
+      />
+      <Route
+        path="/forbidden"
+        element={
+          <RequireUser>
+            <Forbidden />
+          </RequireUser>
+        }
+      />
       <Route path="/internal" element={<InternalServerError />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
