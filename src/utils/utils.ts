ccdb70ca3b668440b6bb3929d3050ba174c9dc8e
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-07-13 15:41:05 +0300

    [feature] implemented utils function for home page

---
+++ src/utils/utils.ts
@@ -0,0 +1,46 @@
+/**
+ * Module for Utility Functions
+ * @module utils
+ */
+
+import { THEME_MODES } from "@/features/base/constants";
+
+/**
+ * Evaluate for dark theme className
+ * @param {string} className
+ * @param {string} theme
+ * @returns {string} string
+ */
+export const darkThemeClass = (
+  className: string,
+  theme: string | null,
+): string => {
+  return theme === THEME_MODES.DARK_THEME ? className : "";
+};
+
+/**
+ * @param {string} imgName
+ * @param {string} ext optional
+ * @returns {string} absolute image path
+ * @example
+ *  imagePath(cloud)
+ *    // => http://localhost:3000/icons/cloud.png
+ */
+export const imagePath = (imgName: string, ext: string = "png"): string => {
+  let url: string;
+  if (import.meta.env.PROD) {
+    url = import.meta.env.REACT_APP_HOSTED_URL;
+  } else {
+    url = `http://localhost:3000`;
+  }
+  return `${url}/icons/${imgName}.${ext}`;
+};
+
+/**
+ * Check if the browser default theme is dark
+ *
+ * @returns {boolean} boolean
+ */
+export const isBrowserDefaultDark = (): boolean => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches;
+};
