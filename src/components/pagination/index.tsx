86068807e1d997b4dcf668f60e52f2c57a88ca5f
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-07-18 21:16:00 +0300

    [feature] implemented the generic pagination component

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,36 @@
+interface RendererProps {
+  handlePagination: (navigation: string) => void;
+  previous: string | null;
+  next: string | null;
+}
+
+const GenericRenderer = (props: RendererProps) => {
+  return (
+    <div>
+      <section className="d-flex justify-content-center align-items-center mb-5">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination mb-0">
+            <li className="page-item cursor">
+              <span
+                onClick={() => props.handlePagination("previous")}
+                className={`page-link ${!props.previous && "disabled"}`}
+                aria-label="Previous"
+              >
+                <span aria-hidden="true">Previous</span>
+              </span>
+            </li>
+            <li className="page-item cursor">
+              <span
+                onClick={() => props.handlePagination("next")}
+                className={`page-link ${!props.next && "disabled"}`}
+                aria-label="Next"
+              >
+                <span aria-hidden="true">Next</span>
+              </span>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </div>
+  );
+};
