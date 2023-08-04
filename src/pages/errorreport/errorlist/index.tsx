6821dec6fcd16cd04f4c691ea9d3d06c30ff35c1
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-08-04 20:17:05 +0300

    [feature] implemented error report list view page & component

---
+++ src/pages/errorreport/errorlist/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useAppDispatch } from "@/app/hooks";
+import MainLayout from "@/components/layout/main";
+import ErrorReportQuery from "@/components/errorreport/ErrorReportQuery";
+import ErrorReportTable from "@/components/errorreport/ErrorReportTable";
+// import { listHarvesters } from "@/features/harvester/harvesterSlice";
+import { queryLocation } from "@/features/location/locationSlice";
+import {
+  copyQueryUrl,
+  queryErrorReport,
+} from "@/features/errorreport/errorreportSlice";
+import { Pagination } from "@/components/pagination";
+import { copiedUrl, paramsToObject } from "@/utils/utils";
+import { CopyToClipboard } from "@/components/copytoclipboard";
+import { MAX_LIMIT } from "@/features/base/constants";
+import { queryFruit } from "@/features/harvester/harvesterSlice";
+import { queryExceptionCode } from "@/features/exception/exceptionSlice";
+// import { listUsers } from "@/features/user/userSlice";
+import { Header } from "@/components/common";
+import "./styles.css";
+
+function ErrorReportListView() {
+  const dispatch = useAppDispatch();
+  const { search } = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      await Promise.all([
+        // dispatch(listHarvesters(MAX_LIMIT)),
+        dispatch(queryLocation({ limit: MAX_LIMIT })),
+        dispatch(queryFruit({ limit: MAX_LIMIT })),
+        dispatch(queryExceptionCode({ limit: MAX_LIMIT })),
+        // dispatch(listUsers(MAX_LIMIT)),
+      ]);
+    })();
+    if (search) {
+      const paramsObj = paramsToObject(search);
+      dispatch(queryErrorReport(paramsObj));
+      dispatch(copyQueryUrl(copiedUrl(paramsObj)));
+    } else {
+      dispatch(queryErrorReport({ is_emulator: 0 }));
+    }
+    return () => {};
+  }, [dispatch, search]);
+
+  return (
+    <MainLayout>
+      <div className="container">
+        <div>
+          <Header
+            title={"HDS Error Reports"}
+            className={"display-6 mt-4 mb-4"}
+          />
+        </div>
+        <ErrorReportQuery />
+        <ErrorReportTable />
+        <Pagination />
+        <CopyToClipboard />
+      </div>
+    </MainLayout>
+  );
+}
+
+export default ErrorReportListView;
