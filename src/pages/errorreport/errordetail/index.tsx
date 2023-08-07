ad3bef2a4410b774f55df5a36226e6eaf664dd02
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-08-07 17:30:16 +0300

    [feature] implemented error report detail view page & components

---
+++ src/pages/errorreport/errordetail/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { CustomBackButton, Header, Loader } from "@/components/common";
+import ErrorReportDetail from "@/components/errorreport/ErrorReportDetail";
+import MainLayout from "@/components/layout/main";
+import { LoaderDiv } from "@/components/styled";
+import { getErrorReport } from "@/features/errorreport/errorreportSlice";
+import { paramsToObject } from "@/utils/utils";
+import "./styles.css";
+
+function ErrorReportDetailView() {
+  const { loading } = useAppSelector((state) => state.errorreport);
+  const { theme } = useAppSelector((state) => state.home);
+  const params = useParams();
+  const dispatch = useAppDispatch();
+  const { search } = useLocation();
+  const paramsObj = paramsToObject(search);
+
+  useEffect(() => {
+    dispatch(getErrorReport(Number(params.reportId)));
+  }, [dispatch, params]);
+
+  return (
+    <MainLayout>
+      <div className="container">
+        <div>
+          <CustomBackButton
+            routeTo="errorreports"
+            theme={theme as string}
+            paramsObj={paramsObj}
+          />
+          <Header
+            title={"HDS Prototype: Error Reports"}
+            className={"display-6 mb-4"}
+            id={params.reportId}
+          />
+        </div>
+
+        {loading ? (
+          <LoaderDiv>
+            <Loader size={50} />
+          </LoaderDiv>
+        ) : (
+          <ErrorReportDetail />
+        )}
+      </div>
+    </MainLayout>
+  );
+}
+
+export default ErrorReportDetailView;
