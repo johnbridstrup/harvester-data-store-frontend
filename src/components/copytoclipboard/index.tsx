1cfaaf274f9eaac5f08dc17ff7da933fd5970141
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-08-08 23:14:10 +0300

    [feature] implement the error pareto copy build configs

--- src/components/copytoclipboard/index.tsx
+++ src/components/copytoclipboard/index.tsx
@@ -4,6 +4,17 @@ import { ClipboardDiv } from "../styled";
 import { darkThemeClass } from "@/utils/utils";
 import { API_URL } from "@/features/base/constants";
 
+interface ConfigProps {
+  paretoArr: Array<{
+    id: string;
+    paretos: { xlabels: Array<string>; ydata: Array<number> };
+    aggregate_query: string;
+    chart_title: string;
+  }>;
+  paramsObj: Record<string, any>;
+  theme: string;
+}
+
 export function CopyToClipboard() {
   const [copied, setCopied] = useState(false);
   const { queryUrl } = useAppSelector((state) => state.errorreport);
@@ -39,3 +50,41 @@ export function CopyToClipboard() {
     </ClipboardDiv>
   );
 }
+
+export const CopyBuildConfig = (props: ConfigProps) => {
+  const [copied, setCopied] = useState(false);
+  const btn = darkThemeClass("btn-dark", props.theme);
+  const copy = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+    } catch (error) {
+      console.log("error", error);
+    }
+    setTimeout(function () {
+      setCopied(false);
+    }, 3000);
+  };
+
+  const buildConfig = async () => {
+    let config = props?.paretoArr.slice().map((pareto) => {
+      return pareto.aggregate_query;
+    });
+    props.paramsObj["configs"] = config;
+    let params = new URLSearchParams(props.paramsObj);
+    let public_url =
+      process.env.REACT_APP_HOSTED_URL || "http://localhost:3000";
+    let configUrl = `/errorreports/view/pareto/?${params.toString()}`;
+    await copy(public_url + configUrl);
+  };
+
+  return (
+    <button onClick={buildConfig} className={`btn mx-2 ${btn}`}>
+      {copied ? (
+        <span className="las la-check-double text-success">copied</span>
+      ) : (
+        "copy config"
+      )}
+    </button>
+  );
+};
