1afa026695a8124b792d7bbb1e8fb33ad59bcad2
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-08-31 17:40:45 +0300

    [refactor]  declutter home menu apps to most common apps

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,24 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "@/app/hooks";
 import MainLayout from "@/components/layout/main";
-import { menu, adminMenu } from "@/assets/menu";
+import { homeMenu } from "@/assets/menu";
 import LandingView from "@/components/home";
 import { darkThemeClass, imagePath } from "@/utils/utils";
 import "./styles.css";
 
 function Home() {
-  const { user } = useAppSelector((state) => state.auth);
   const { theme } = useAppSelector((state) => state.home);
-  const menus = user?.is_superuser ? adminMenu : menu;
   const cardtheme = darkThemeClass("dt-home-card-theme", theme);
 
   return (
     <MainLayout>
       <div className="container">
         <div className="mb-5">
-          <h2 className="display-4">Welcome to HDS</h2>
+          <h2 className="display-4">Harvester Data Store</h2>
         </div>
         <LandingView theme={theme} />
         <div className="row mb-4">
-          {menus.map((item, index) => (
+          {homeMenu.map((item, index) => (
             <Link
               to={item.href}
               key={index}
