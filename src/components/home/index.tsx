18ad6b7bb89e394b77f5f521de75f02ad8acb5fd
kkthecompguy <hnkhosamomollo3@gmail.com>
2023-09-20 15:05:16 +0300

    [refactor] isolate all jsx to landing view component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,16 +1,41 @@
+import { Link } from "react-router-dom";
+import { useAppSelector } from "@/app/hooks";
+import { homeMenu } from "@/assets/menu";
 import SearchHarvester from "./searchharvester";
+import { darkThemeClass, imagePath } from "@/utils/utils";
 
-interface HomeProps {
-  theme: string | null;
-}
-
-function LandingView(props: HomeProps) {
+function LandingView() {
+  const { theme } = useAppSelector((state) => state.home);
+  const cardtheme = darkThemeClass("dt-home-card-theme", theme);
   return (
-    <div className="row">
-      <div className="col-md-6 mx-auto">
-        <SearchHarvester component="homepage" theme={props.theme} />
+    <>
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <SearchHarvester component="homepage" theme={theme} />
+        </div>
       </div>
-    </div>
+      <div className="row mb-4">
+        {homeMenu.map((item, index) => (
+          <Link
+            to={item.href}
+            key={index}
+            className="col-md-4 link-secondary mt-4"
+          >
+            <div className={`card card-body hover1 ${cardtheme}`}>
+              <div className="link-icon-container">
+                <img
+                  className="menu-icon"
+                  src={imagePath(item.icon)}
+                  alt={`${item.icon}`}
+                />
+                <span className="link">{item.name}</span>
+              </div>
+              <div>{item.description}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,43 +1,15 @@
-import { Link } from "react-router-dom";
-import { useAppSelector } from "@/app/hooks";
 import MainLayout from "@/components/layout/main";
-import { homeMenu } from "@/assets/menu";
 import LandingView from "@/components/home";
-import { darkThemeClass, imagePath } from "@/utils/utils";
 import "./styles.css";
 
 function Home() {
-  const { theme } = useAppSelector((state) => state.home);
-  const cardtheme = darkThemeClass("dt-home-card-theme", theme);
-
   return (
     <MainLayout>
       <div className="container">
         <div className="mb-5">
           <h2 className="display-4">Harvester Data Store</h2>
         </div>
-        <LandingView theme={theme} />
-        <div className="row mb-4">
-          {homeMenu.map((item, index) => (
-            <Link
-              to={item.href}
-              key={index}
-              className="col-md-4 link-secondary mt-4"
-            >
-              <div className={`card card-body hover1 ${cardtheme}`}>
-                <div className="link-icon-container">
-                  <img
-                    className="menu-icon"
-                    src={imagePath(item.icon)}
-                    alt={`${item.icon}`}
-                  />
-                  <span className="link">{item.name}</span>
-                </div>
-                <div>{item.description}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <LandingView />
       </div>
     </MainLayout>
   );
